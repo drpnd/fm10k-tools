ad9277723121076fab1d11c70afc76fd91a474ff
Hirochika Asai <panda@jar.jp>
2018-01-05 17:20:44 +0900

    Update header.

--- fm10k.h
+++ fm10k.h
@@ -24,35 +24,35 @@
 #ifndef _FM10K_H
 #define _FM10K_H
 
-#define FM10K_EPL(a)            (0x0e0000 + (a) * 4)
-#define FM10K_PARSER(a)         (0xcf0000 + (a) * 4)
-#define FM10K_FFU(a)            (0xc00000 + (a) * 4)
-#define FM10K_FFU_MAP(a)        (0xda0000 + (a) * 4)
-#define FM10K_EACL(a)           (0xdb0000 + (a) * 4)
-#define FM10K_ARP(a)            (0xcc0000 + (a) * 4)
-#define FM10K_POLICER_APPLY(a)  (0xd60000 + (a) * 4)
-#define FM10K_POLICER_USAGE(a)  (0xe40000 + (a) * 4)
-#define FM10K_L2LOOKUP(a)       (0xc80000 + (a) * 4)
-#define FM10K_L2LOOKUP_TCN(a)   (0xe70000 + (a) * 4)
-#define FM10K_HANDLER(a)        (0xd50000 + (a) * 4)
-#define FM10K_HANDLER_TAIL(a)   (0xe30000 + (a) * 4)
-#define FM10K_GLORT(a)          (0xce0000 + (a) * 4)
-#define FM10K_LAG(a)            (0xd90000 + (a) * 4)
-#define FM10K_TRIG_APPLY(a)     (0xd70000 + (a) * 4)
-#define FM10K_TRIG_USAGE(a)     (0xe78000 + (a) * 4)
-#define FM10K_CM_APPLY(a)       (0xd40000 + (a) * 4)
-#define FM10K_CM_USAGE(a)       (0xe60000 + (a) * 4)
-#define FM10K_MOD(a)            (0xe80000 + (a) * 4)
-#define FM10K_RX_STATS(a)       (0xe00000 + (a) * 4)
-#define FM10K_SCHED(a)          (0xf00000 + (a) * 4)
-#define FM10K_FIBM(a)           (0x008000 + (a) * 4)
-#define FM10K_MGMT(a)           (0x000000 + (a) * 4)
-#define FM10K_PORTS_MGMT(a)     (0x0e8000 + (a) * 4)
-#define FM10K_PCIE_PF(a)        (0x100000 + (a) * 4)
-#define FM10K_PCIE_VF(a)        (0x200000 + (a) * 4)
-#define FM10K_PCIE_CFG(a)       (0x120000 + (a) * 4)
-#define FM10K_PCIE_CFG_VF(a)    (0x130000 + (a) * 4)
-#define FM10K_TE(a)             (0xa00000 + (a) * 4)
+#define FM10K_EPL(a)            ((0x0e0000 + (a)) * 4)
+#define FM10K_PARSER(a)         ((0xcf0000 + (a)) * 4)
+#define FM10K_FFU(a)            ((0xc00000 + (a)) * 4)
+#define FM10K_FFU_MAP(a)        ((0xda0000 + (a)) * 4)
+#define FM10K_EACL(a)           ((0xdb0000 + (a)) * 4)
+#define FM10K_ARP(a)            ((0xcc0000 + (a)) * 4)
+#define FM10K_POLICER_APPLY(a)  ((0xd60000 + (a)) * 4)
+#define FM10K_POLICER_USAGE(a)  ((0xe40000 + (a)) * 4)
+#define FM10K_L2LOOKUP(a)       ((0xc80000 + (a)) * 4)
+#define FM10K_L2LOOKUP_TCN(a)   ((0xe70000 + (a)) * 4)
+#define FM10K_HANDLER(a)        ((0xd50000 + (a)) * 4)
+#define FM10K_HANDLER_TAIL(a)   ((0xe30000 + (a)) * 4)
+#define FM10K_GLORT(a)          ((0xce0000 + (a)) * 4)
+#define FM10K_LAG(a)            ((0xd90000 + (a)) * 4)
+#define FM10K_TRIG_APPLY(a)     ((0xd70000 + (a)) * 4)
+#define FM10K_TRIG_USAGE(a)     ((0xe78000 + (a)) * 4)
+#define FM10K_CM_APPLY(a)       ((0xd40000 + (a)) * 4)
+#define FM10K_CM_USAGE(a)       ((0xe60000 + (a)) * 4)
+#define FM10K_MOD(a)            ((0xe80000 + (a)) * 4)
+#define FM10K_RX_STATS(a)       ((0xe00000 + (a)) * 4)
+#define FM10K_SCHED(a)          ((0xf00000 + (a)) * 4)
+#define FM10K_FIBM(a)           ((0x008000 + (a)) * 4)
+#define FM10K_MGMT(a)           ((0x000000 + (a)) * 4)
+#define FM10K_PORTS_MGMT(a)     ((0x0e8000 + (a)) * 4)
+#define FM10K_PCIE_PF(a)        ((0x100000 + (a)) * 4)
+#define FM10K_PCIE_VF(a)        ((0x200000 + (a)) * 4)
+#define FM10K_PCIE_CFG(a)       ((0x120000 + (a)) * 4)
+#define FM10K_PCIE_CFG_VF(a)    ((0x130000 + (a)) * 4)
+#define FM10K_TE(a)             ((0xa00000 + (a)) * 4)
 
 
 /*
@@ -159,6 +159,123 @@
  */
 #define FM10K_SCHED_FREELIST_INIT       FM10K_SCHED(0x30244)
 
+/*
+ * PLL_FABRIC_CTRL
+ * 0     Nreset
+ * 1     Enable
+ * 2     Halt
+ * 8:3   RefDiv
+ * 9     FbDiv4
+ * 17:10 FbDiv255
+ * 23:18 OutDiv
+ * 26:24 OutMuxSel
+ * 31:27 Reserved
+ */
+#define FM10K_PLL_FABRIC_CTRL   FM10K_PORTS_MGMT(0x2)
+
+/*
+ * PLL_FABRIC_STAT
+ * 0     PllLocked
+ * 1     PllFreqChange
+ * 9:2   MiscCtrl
+ * 31:10 Reserved
+ */
+#define FM10K_PLL_FABRIC_STAT   FM10K_PORTS_MGMT(0x3)
+
+
+/*
+ * PLL_FABRIC_LOCK
+ * 3:0  FeatureCode
+ * 7:4  FreqSel
+ * 31:8 Reserved
+ */
+#define FM10K_PLL_FABRIC_LOCK   FM10K_PORTS_MGMT(0x4)
+
+/*
+ * SBUS_EPL_CFG
+ * 0    SBUS_ControllerReset
+ * 1    RomEnable
+ * 2    RomBusy
+ * 3    BistDonePass
+ * 4    BistDoneFail
+ * 31:5 Reserved
+ */
+#define FM10K_SBUS_EPL_CFG      FM10K_PORTS_MGMT(0x5)
+
+/*
+ * PLL_PCIE_CTRL
+ * 0     Nreset
+ * 1     Enable
+ * 2     Halt
+ * 8:3   RefDiv
+ * 9     FbDiv4
+ * 17:10 FbDiv255
+ * 23:18 OutDiv
+ * 26:24 OutMuxSel
+ * 31:27 Reserved
+ */
+#define FM10K_PLL_PCIE_CTRL     FM10K_MGMT(0x2241)
+
+/*
+ * PLL_PCIE_STAT
+ * 0     PllLocked
+ * 1     PllFreqChange
+ * 9:2   MiscCtrl
+ * 31:10 Reserved
+ */
+#define FM10K_PLL_PCIE_STAT     FM10K_MGMT(0x2242)
+
+/*
+ * PCIE_CLK_CTRL
+ * 3:0   Nreset[0..3]
+ * 7:4   Enable[0..3]
+ * 11:8  Halt[0..3]
+ * 19:12 OutMuxSel[0..3] (4 x 2-bit)
+ * 31:20 Mode[0..3] (4 x 3-bit)
+ */
+#define FM10K_PCIE_CLK_CTRL     FM10K_MGMT(0x3001)
+
+/*
+ * PCIE_CLK_CTRL2
+ * 3:0  XclkTerm[0..3]
+ * 7:4  ClkObs
+ * 31:8 Reserved
+ */
+#define FM10K_PCIE_CLK_CTRL2    FM10K_MGMT(0x3002)
+
+/*
+ * DEVICE_CFG
+ * 3:0   PCIeMode[0..3]
+ * 4     Eth100GDisabled
+ * 6:5   FeatureCode
+ * 15:7  PCIeEnable[0..8]
+ * 16    SystemClockSource
+ * 31:17 Reserved
+ */
+#define FM10K_DEVICE_CFG        FM10K_MGMT(0x4)
+
+/*
+ * SOFT_RESET
+ * 0     ColdReset
+ * 1     EPLReset
+ * 2     SwitchReset
+ * 3     SwitchReady
+ * 12:4  PCIeReset
+ * 21:13 PCIeActive
+ * 31:22 Reserved
+ */
+#define FM10K_SOFT_RESET        FM10K_MGMT(0x3)
+
+/*
+ * SBUS_PCIE_CFG
+ * 0    SBUS_ControllerReset
+ * 1    RomEnable
+ * 2    RomBusy
+ * 3    BistDonePass
+ * 4    BistDoneFail
+ * 31:5 Reserved
+ */
+#define FM10K_SBUS_PCIE_CFG     FM10K_MGMT(0x2243)
 
 #ifdef __cplusplus
 extern "C" {
